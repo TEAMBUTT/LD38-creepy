ae97854478c9b2cb32c0c0896e14e20aaceec5e8
Jared Norman <jared@creepywizard.com>
2017-04-18 19:37:40 -0700

    Add script for packaging for release

---
+++ package.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+love-release \
+  -t "Game Name" \
+  --uti "creepywizard.game" \
+  -a "Creepy Wizard" \
+  -e "jared@creepywizard.com" \
+  -M -W 32 \
+  -b \
+  dist src
--- src/conf.lua
+++ src/conf.lua
@@ -1,8 +1,9 @@
 function love.conf(t)
-  t.version = "0.10.2"                -- The LÖVE version this game was made for (string)
   t.window.title = "Game Name"        -- The window title (string)
   t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
   t.window.width = 800                -- The window width (number)
   t.window.height = 600               -- The window height (number)
   t.window.fullscreen = false         -- Enable fullscreen (boolean)
+
+  t.version = "0.10.2"                -- The LÖVE version this game was made for (string)
 end
