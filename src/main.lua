4af7677b2de93fc9bd02b225cff3ff7031e7a574
Jared Norman <jared@creepywizard.com>
2017-04-21 18:37:19 -0700

    Trigger events

--- src/main.lua
+++ src/main.lua
@@ -1,6 +1,14 @@
 require 'env'
 
+local Portion = require 'flimsy.portion'
+local Event = require 'flimsy.event'
+
+local portions = {}
+local currentPortion
+
 function love.load()
+  portions.game = Portion:new()
+  currentPortion = portions.game
 end
 
 function love.draw()
@@ -8,18 +16,31 @@ function love.draw()
 end
 
 function love.update(deltaTime)
+  currentPortion:processEvent(Event:new('advance_time', deltaTime))
 end
 
 function love.mousepressed(x, y, button)
+  currentPortion:processEvent(Event:new('mouse_press', {
+    x = x,
+    y = y,
+    button = button
+  }))
 end
 
 function love.mousereleased(x, y, button)
+  currentPortion:processEvent(Event:new('mouse_release', {
+    x = x,
+    y = y,
+    button = button
+  }))
 end
 
 function love.keypressed(key)
+  currentPortion:processEvent(Event:new('key_press', key))
 end
 
 function love.keyreleased(key)
+  currentPortion:processEvent(Event:new('key_release', key))
 end
 
 function love.quit()
