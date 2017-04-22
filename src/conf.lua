85d3f5fe68bc9825d601ce65782037249fa40417
Jared Norman <jared@creepywizard.com>
2017-04-21 21:58:30 -0700

    Change screen dimensions

--- src/conf.lua
+++ src/conf.lua
@@ -1,8 +1,8 @@
 function love.conf(t)
   t.window.title = "Game Name"        -- The window title (string)
   t.window.icon = nil                 -- Filepath to an image to use as the window's icon (string)
-  t.window.width = 800                -- The window width (number)
-  t.window.height = 600               -- The window height (number)
+  t.window.width = 1024               -- The window width (number)
+  t.window.height = 768               -- The window height (number)
   t.window.fullscreen = false         -- Enable fullscreen (boolean)
 
   t.version = "0.10.2"                -- The LÖVE version this game was made for (string)
